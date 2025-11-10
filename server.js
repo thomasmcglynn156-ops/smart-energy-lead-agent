3b438ea9eb0fe4ec95989ff2786348f1670fc828
thomasmcglynn156-ops <thomasmcglynn156@gmail.com>
2025-11-10 10:52:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -42,12 +42,15 @@ const upload = multer({
   }
 });
 
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type']
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 
 // Email configuration
